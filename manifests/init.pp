9ed346494ca0d229bf96231c8a9bc653c43e703e
Adriaan Peeters <Adriaan_Peeters@peopleware.be>
2014-03-14 12:06:19 +0100

    Initial migration to jetty8 instead of jetty 6 Debian package

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,26 @@
 class enable_jetty ( $xmx = 256 ) {
 
-  package { 'jetty':
+  package { 'jetty8':
     ensure  => installed,
   }
 
-  service { 'jetty':
+  service { 'jetty8':
     ensure  => running,
     enable  => true,
     require => [
-      Package['jetty'],
-      File['/etc/default/jetty'],
+      Package['jetty8'],
+      File['/etc/default/jetty8'],
     ]
   }
 
-  file { '/etc/default/jetty':
+  file { '/etc/default/jetty8':
     content => template('enable_jetty/default-jetty.erb'),
-    require => Package['jetty'],
+    require => Package['jetty8'],
     owner   => 'root',
     group   => 'root',
   }
 
-  File['/etc/default/jetty'] ~> Service['jetty']
+  File['/etc/default/jetty8'] ~> Service['jetty8']
 
   package { 'openjdk-7-jdk':
     ensure => installed,
